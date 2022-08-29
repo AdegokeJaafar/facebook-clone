304be307a0ec8917c82b78a842f21bdf3a100fa0
Adegoke Jaafar <jafaar@webcoupers.com>
2022-08-29 17:19:41 +0100

    clone

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
-import firebase from "./firebase";
-import  "firebase/storage";
+// import firebase from "./firebase";
+// import  "firebase/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBlC_7vp4D8BGtMxuXkr7WddrccKKM2sR8",
-    authDomain: "facebook-clone-36d21.firebaseapp.com",
-    projectId: "facebook-clone-36d21",
-    storageBucket: "facebook-clone-36d21.appspot.com",
-    messagingSenderId: "768377095101",
-    appId: "1:768377095101:web:adbe9b5b618933334808d4"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBlC_7vp4D8BGtMxuXkr7WddrccKKM2sR8",
+//     authDomain: "facebook-clone-36d21.firebaseapp.com",
+//     projectId: "facebook-clone-36d21",
+//     storageBucket: "facebook-clone-36d21.appspot.com",
+//     messagingSenderId: "768377095101",
+//     appId: "1:768377095101:web:adbe9b5b618933334808d4"
+//   };
 
-  const app = !firebase?.apps.length ? firebase?.initializeApp(firebaseConfig) : firebase.app();
+//   const app = !firebase?.apps.length ? firebase?.initializeApp(firebaseConfig) : firebase.app();
 
-  const db = app?.firestore();
-  const storage = firebase?.storage();
+//   const db = app?.firestore();
+//   const storage = firebase?.storage();
 
-  export { db, storage };
+//   export { db, storage };
