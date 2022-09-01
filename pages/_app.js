71de983ad5bec0aca5ac90fa57513e8bb8fb8b19
Adegoke Jaafar <jafaar@webcoupers.com>
2022-09-01 11:51:16 +0100

    clone

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps, session}) {
+export default function App({
+  Component, pageProps: { session, ...pageProps }
+}) {
   return (
     <SessionProvider session={session}>
-  <Component {...pageProps} />
-  </SessionProvider>
-  );
+      <Component {...pageProps}/>
+    </SessionProvider>
+  )
 }
 
-export default MyApp
+
