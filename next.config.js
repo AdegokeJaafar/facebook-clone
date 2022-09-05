650ae1975b280a8699b2bb8c2be94d0d2ea8122f
Adegoke Jaafar <jafaar@webcoupers.com>
2022-09-05 13:38:53 +0100

    clone

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports =  {
   env: {
     FACEBOOK_CLIENT_ID: "448689343941383",
     FACEBOOK_CLIENT_SECRET: "238d96db0b5f95b3d88c8906f8f0b09f",
+    SECRET: "fd14e877e46be86a46b22cb7909e78f2",
     NEXTAUTH_URL:  "https://facebook-clone-delta-ecru.vercel.app/"
   }
 };
