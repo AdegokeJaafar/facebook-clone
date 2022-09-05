838789b44bf9e120c530e44724085d33cd7c9f0a
Adegoke Jaafar <jafaar@webcoupers.com>
2022-09-05 12:10:37 +0100

    clone

--- next.config.js
+++ next.config.js
@@ -5,12 +5,12 @@ module.exports =  {
       "platform-lookaside.fbsbx.com",
       "firebasestorage.googleapis.com",
     ],
-  },
-  env: {
-    FACEBOOK_CLIENT_ID: "448689343941383",
-    FACEBOOK_CLIENT_SECRET: "238d96db0b5f95b3d88c8906f8f0b09f",
-    NEXTAUTH_URL:  "https://facebook-clone-delta-ecru.vercel.app/"
   }
-
 };
 
+// ,
+//   env: {
+//     FACEBOOK_CLIENT_ID: "448689343941383",
+//     FACEBOOK_CLIENT_SECRET: "238d96db0b5f95b3d88c8906f8f0b09f",
+//     NEXTAUTH_URL:  "https://facebook-clone-delta-ecru.vercel.app/"
+//   }
